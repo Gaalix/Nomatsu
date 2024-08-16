6f1619934d3458c54e5c006719cafcd60bbe6031
Gaalix <134967079+Gaalix@users.noreply.github.com>
2024-08-16 12:39:59 +0200

    fix to allowedHosts being []

--- nomatsu/public/electron.js
+++ nomatsu/public/electron.js
@@ -1,6 +1,7 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+const url = require("url");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -12,22 +13,30 @@ function createWindow() {
     },
   });
 
-  win.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
+  const startUrl = isDev
+    ? "http://localhost:3000"
+    : url.format({
+        pathname: path.join(__dirname, "../build/index.html"),
+        protocol: "file:",
+        slashes: true,
+      });
+
+  win.loadURL(startUrl);
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
